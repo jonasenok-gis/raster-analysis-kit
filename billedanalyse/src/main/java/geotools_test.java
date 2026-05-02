6ae19470a91be8c1cdc8e2a93f6943b621689c01
Jonas <jonasenok@gmail.com>
2026-05-02 10:46:35 +0200

    tilføjet lille eksempel på en pipeline

--- billedanalyse/src/main/java/geotools_test.java
+++ billedanalyse/src/main/java/geotools_test.java
@@ -2,7 +2,7 @@ import java.io.File;
 import org.geotools.coverage.grid.GridCoverage2D;
 import org.geotools.gce.geotiff.GeoTiffReader;
 
-public class RasterExample {
+public class geotools_test {
     public static void main(String[] args) throws Exception {
 
         File file = new File("../DTM_1km_6192_704.tif");
